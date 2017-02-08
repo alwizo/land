033697ce7d7b083c323ed7373d0b116f6c35e4de
alex <alex@alexs-MacBook-Air.local>
2017-02-08 19:12:37 +0200

    Initial commit

---
+++ database/migrations/2017_02_08_161426_create_table_portfolios.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTablePortfolios extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('portfolios', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('images');
+            $table->string('filter');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('portfolios');
+    }
+}
